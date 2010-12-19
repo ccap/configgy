2162a3329e9cd2c7130bfd372398b267601c79fd
Derek Williams <derek@nebvin.ca>
2010-12-19 16:30:05 -0700

    Added link to github repository

--- project/build/ConfiggyProject.scala
+++ project/build/ConfiggyProject.scala
@@ -5,13 +5,16 @@ class ConfiggyProject(info: ProjectInfo) extends StandardProject(info) with Subv
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.6" % "test"
 
   override def pomExtra =
+    (<name>Configgy</name>
+    <description>Configgy logging removed</description>
+    <url>http://github.com/derekjw/configgy</url>
     <licenses>
       <license>
         <name>Apache 2</name>
         <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
         <distribution>repo</distribution>
       </license>
-    </licenses>
+    </licenses>)
 
   override def subversionRepository = Some("http://svn.local.twitter.com/maven-public")
 }
