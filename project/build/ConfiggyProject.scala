2de09887ed4a67b4898c9ecd85361080387f4baf
Bruce Mitchener <bruce.mitchener@gmail.com>
2011-05-24 10:54:58 +0700

    Update to specs 1.6.8 as that works with Scala 2.9.0.

--- project/build/ConfiggyProject.scala
+++ project/build/ConfiggyProject.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 class ConfiggyProject(info: ProjectInfo) extends DefaultProject(info) {
-  val specs = "org.scala-tools.testing" %% "specs" % "1.6.6" % "test"
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.8" % "test"
 
   override def pomExtra =
     (<name>Configgy</name>
